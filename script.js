a278ae1ddc74392c41175d608ef58a9bf28f597b
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-07 12:10:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+var para = document.querySelector("#status");
+var btn = document.querySelector("#enterBtn")
+
+btn.addEventListener("click",function() {
+	para.innerHTML = "<h1>Entered Metaverse</h1>";
+})
+
